8e9a0d8c2947444360daaad3a914bf7427355522
suriagy <727495945@qq.com>
2018-11-26 17:11:39 +0800

    init

---
+++ src/components/cusBreadcrumb.vue
@@ -0,0 +1,19 @@
+<template>
+ <!-- 面包屑 -->
+      <el-breadcrumb separator="/">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>{{this.level1}}</el-breadcrumb-item>
+          <el-breadcrumb-item>{{this.level2}}</el-breadcrumb-item>
+      </el-breadcrumb>
+</template>
+
+<script>
+export default {
+  name: 'cusBreadcrumb',
+  props: ['level1', 'level2']
+}
+</script>
+
+<style>
+
+</style>
